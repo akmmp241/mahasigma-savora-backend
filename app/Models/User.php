5081809685cf69b6f830bed274e77f176fc87537
Akmal Muhammad Pridianto <akmalmp241@gmail.com>
2024-12-07 01:38:50 +0700

    feat: :arrow_up: install laravel api

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\Contracts\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements HasApiTokens
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, \Laravel\Sanctum\HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
