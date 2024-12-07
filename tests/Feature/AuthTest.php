ff942ff94107d14803ae9824f47b17c1e2cb7d1a
Akmal Muhammad Pridianto <akmalmp241@gmail.com>
2024-12-07 21:19:05 +0700

    feat: :white_check_mark: add test for get user and logout

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\User;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
@@ -81,5 +82,63 @@ class AuthTest extends TestCase
 
         $response->assertStatus(400);
         print_r($response->getContent());
+    }
+
+    public function test_get_user()
+    {
+        $user = User::query()->create([
+            'email' => 'admin@savora.co.id',
+            'password' => '12345678Ab',
+            'name' => 'savora'
+        ]);
+
+        Sanctum::actingAs($user);
+
+        $response = $this->get('/api/v1/auth/user');
+
+        print_r('response' . $response->getContent());
+        $response->assertStatus(200);
+        $response->assertSeeText('admin@savora.co.id');
+    }
+
+    public function test_get_user_unauthenticated()
+    {
+        $response = $this
+            ->withHeaders([
+                'Accept' => 'application/json',
+            ])
+            ->get('/api/v1/auth/user');
+
+        print_r('response' . $response->getContent());
+        $response->assertStatus(401);
+        $response->assertSeeText("Unauthenticated");
+    }
+
+    public function test_logout()
+    {
+        $user = User::query()->create([
+            'email' => 'admin@savora.co.id',
+            'password' => '12345678Ab',
+            'name' => 'savora'
+        ]);
+
+        Sanctum::actingAs($user);
+
+        $response = $this->delete('/api/v1/auth/logout');
+
+        print_r($response->getContent());
+        $response->assertStatus(200);
+    }
+
+    public function test_logout_unauthenticated()
+    {
+        $response = $this
+            ->withHeaders([
+                'Accept' => 'application/json'
+            ])
+            ->delete('/api/v1/auth/logout');
+
+        print_r($response->getContent());
+        $response->assertStatus(401);
     }
 }
