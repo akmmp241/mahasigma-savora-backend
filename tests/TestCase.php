5b590b16ecf338e16a63f84b578c048295de452e
Akmal Muhammad Pridianto <akmalmp241@gmail.com>
2024-12-08 15:20:58 +0700

    feat: :white_check_mark: add test for foods

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,6 +10,8 @@ abstract class TestCase extends BaseTestCase
     protected function setUp(): void
     {
         parent::setUp();
+        DB::delete('delete from foods');
+        DB::delete('delete from vendors');
         DB::delete("delete from users");
         DB::delete('delete from categories');
     }
